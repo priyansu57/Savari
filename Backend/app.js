260680319f382a570327e70cc59927307f1552bb
priyansu57 <priyansupriyajyotij@gmail.com>
2025-09-12 12:29:23 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -16,7 +16,7 @@ const rideRoutes = require("./routes/ride.routes.js");
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://savari-2.onrender.com",
     credentials:true
 }));
 
